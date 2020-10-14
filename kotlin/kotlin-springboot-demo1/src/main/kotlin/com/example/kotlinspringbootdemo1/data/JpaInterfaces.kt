cea3ab4fd1e003ef3cd554223e3f7d5a9d731a9c
Joaquim Costa <jscosta88@gmail.com>
2020-10-13 23:45:03 -0400

    cleanup

--- kotlin/kotlin-springboot-demo1/src/main/kotlin/com/example/kotlinspringbootdemo1/data/JpaInterfaces.kt
+++
@@ -1,8 +0,0 @@
-package com.example.kotlinspringbootdemo1.data
-
-import com.example.kotlinspringbootdemo1.model.Seat
-import org.springframework.data.repository.CrudRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface SeatRepository :CrudRepository<Seat,Long>
